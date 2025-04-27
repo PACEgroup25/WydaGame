39aca25bc88d8814dfa79f07f4cfe433d4d0693e
NoJc <nojac42@gmail.com>
2025-04-27 22:24:34 +1000

    Create files for analytics view components, mirror nav bar structure in root svelte file

--- wyda-app/src/routes/+page.svelte
+++ wyda-app/src/routes/+page.svelte
@@ -1,3 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>hello</p>
+<script>
+  import WydaAdmin from "../analyticsViews/WydaAdmin.svelte";
+  import ClientAdmin from "../analyticsViews/ClientAdmin.svelte";
+  import Learner from "../analyticsViews/Learner.svelte";
+  import LearningCoach from "../analyticsViews/LearningCoach.svelte";
+</script>
+
+<main>
+  <div class="navigation-container">
+    <div class="logo-container"></div>
+    <div class="locations-container"></div>
+    <div class="profile-container"></div>
+    <div class="actions-container"></div>
+  </div>
+  <div class="dashboard"></div>
+</main>
