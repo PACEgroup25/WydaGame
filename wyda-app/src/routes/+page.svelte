3110b4fceaca0d13bf6e5755a03933f8ccc6455b
NoJc <nojac42@gmail.com>
2025-04-30 01:14:54 +1000

    Extract sidebar into a svelte component

--- wyda-app/src/routes/+page.svelte
+++ wyda-app/src/routes/+page.svelte
@@ -6,76 +6,36 @@
   import { Button } from "../lib/components/ui/button";
   import * as RadioGroup from "$lib/components/ui/radio-group/index";
   import { Label } from "$lib/components/ui/label";
-  import {
-    LayoutDashboard,
-    Info,
-    ChartLine,
-    LogOut,
-    Bell,
-  } from "@lucide/svelte";
   let role = "learner";
+  import SideBar from "$lib/components/ui/side-bar/SideBar.svelte";
 </script>
 
 <main>
   <div class="page-container flex flex-row">
-    <div
-      class="navigation-container bg-[#f0f2f6] border-r border-r-greyBorder h-100dvh flex flex-col"
-    >
-      <div class="logo-container flex flex-row justify-center mt-4 mb-4">
-        <img src="/full_logo.png" alt="wyda-logo" class="h-7" />
-      </div>
-      <div class="locations-container flex flex-col h-full mt-8 mb-4">
-        <Button variant="sidebar" class="m-1 flex justify-start">
-          <LayoutDashboard />
-          <span>Dashboard</span></Button
-        >
-        <Button variant="sidebar" class="m-1 flex justify-start">
-          <Info />
-          <span>Help</span>
-        </Button>
-        <Button variant="sidebar" class="m-1 flex justify-start">
-          <ChartLine />
-          <span>Analytics</span>
-        </Button>
-        <div class="view-switcher flex justify-center mb-12 mt-8">
-          <RadioGroup.Root bind:value={role}>
-            <p class="text-center">Temporary View Switcher</p>
-            <div class="flex items-center space-x-2">
-              <RadioGroup.Item value="learner" id="r1" />
-              <Label for="r1">Learner View</Label>
-            </div>
-            <div class="flex items-center space-x-2">
-              <RadioGroup.Item value="wydaAdmin" id="r2" />
-              <Label for="r2">Wyda Admin View</Label>
-            </div>
-            <div class="flex items-center space-x-2">
-              <RadioGroup.Item value="clientAdmin" id="r3" />
-              <Label for="r3">Client Admin View</Label>
-            </div>
-            <div class="flex items-center space-x-2">
-              <RadioGroup.Item value="learningCoach" id="r4" />
-              <Label for="r4">Learning Coach View</Label>
-            </div>
-          </RadioGroup.Root>
-        </div>
-      </div>
-      <div class="profile-container flex justify-evenly mb-4">
-        <div
-          class="image-container flex justify-center items-center h-12 w-12 bg-blue-500 rounded-full"
-        >
-          JD
-        </div>
-        <div class="text-container flex items-center">John Doe</div>
-      </div>
-      <div
-        class="actions-container flex justify-evenly border-t border-t-greyBorder"
-      >
-        <Button variant="sidebar" class="m-1 w-[6em]"><LogOut /></Button>
-        <Button variant="sidebar" class="m-1 w-[6em]"><Bell /></Button>
-      </div>
-    </div>
+    <SideBar />
     <!-- NOTE: roles are to be sourced from JWT Tokens in final product -->
     <div class="dashboard flex items-center justify-center h-100dvh w-full">
+      <div class="view-switcher flex justify-center mb-12 mt-8">
+        <RadioGroup.Root bind:value={role}>
+          <p class="text-center">Temporary View Switcher</p>
+          <div class="flex items-center space-x-2">
+            <RadioGroup.Item value="learner" id="r1" />
+            <Label for="r1">Learner View</Label>
+          </div>
+          <div class="flex items-center space-x-2">
+            <RadioGroup.Item value="wydaAdmin" id="r2" />
+            <Label for="r2">Wyda Admin View</Label>
+          </div>
+          <div class="flex items-center space-x-2">
+            <RadioGroup.Item value="clientAdmin" id="r3" />
+            <Label for="r3">Client Admin View</Label>
+          </div>
+          <div class="flex items-center space-x-2">
+            <RadioGroup.Item value="learningCoach" id="r4" />
+            <Label for="r4">Learning Coach View</Label>
+          </div>
+        </RadioGroup.Root>
+      </div>
       {#if role == "learner"}
         <Learner />
       {:else if role == "wydaAdmin"}
