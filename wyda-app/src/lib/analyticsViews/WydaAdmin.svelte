c773e9c4c9775c2ef7ec34fc7b3ffbab8eea2c56
NoJc <nojac42@gmail.com>
2025-04-30 01:41:33 +1000

    Move analyticsViews inside lib, resolve paths

--- wyda-app/src/lib/analyticsViews/WydaAdmin.svelte
+++ wyda-app/src/lib/analyticsViews/WydaAdmin.svelte
@@ -1,5 +1,5 @@
 <script>
-  import BarChart from "../lib/components/graphs/bar-chart/BarChart.svelte";
+  import BarChart from "$lib/components/graphs/bar-chart/BarChart.svelte";
 </script>
 
 <p class="text-blue-600 text-4xl">Wyda Admin View</p>
--- wyda-app/src/routes/analytics/+page.svelte
+++ wyda-app/src/routes/analytics/+page.svelte
@@ -3,11 +3,11 @@
   import ClientAdmin from "$lib/analyticsViews/ClientAdmin.svelte";
   import Learner from "$lib/analyticsViews/Learner.svelte";
   import LearningCoach from "$lib/analyticsViews/LearningCoach.svelte";
-  import * as RadioGroup from "$lib/components/ui/radio-group/index";
   import { Button } from "$lib/components/ui/button";
+  import * as RadioGroup from "$lib/components/ui/radio-group/index";
   import { Label } from "$lib/components/ui/label";
-  let role = "learner";
   import SideBar from "$lib/components/ui/side-bar/SideBar.svelte";
+  let role = "learner";
 </script>
 
 <main>
