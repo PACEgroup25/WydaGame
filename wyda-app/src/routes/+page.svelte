c28b0dc1fe55007807e64ab92170b637de649b99
NoJc <nojac42@gmail.com>
2025-04-30 16:02:21 +1000

    Add dashboard place holder text

--- wyda-app/src/routes/+page.svelte
+++ wyda-app/src/routes/+page.svelte
@@ -1,18 +1,15 @@
 <script>
-  import WydaAdmin from "$lib/analyticsViews/WydaAdmin.svelte";
-  import ClientAdmin from "$lib/analyticsViews/ClientAdmin.svelte";
-  import Learner from "$lib/analyticsViews/Learner.svelte";
-  import LearningCoach from "$lib/analyticsViews/LearningCoach.svelte";
-  import * as RadioGroup from "$lib/components/ui/radio-group/index";
-  import { Button } from "$lib/components/ui/button";
-  import { Label } from "$lib/components/ui/label";
-  let role = "learner";
   import SideBar from "$lib/components/ui/side-bar/SideBar.svelte";
 </script>
 
 <main>
   <div class="page-container flex flex-row">
     <SideBar />
+    <div
+      class="dashboard flex flex-col items-center justify-center h-100dvh w-full"
+    >
+      <div class="text-blue-600 text-4xl">Dashboard Content Placeholder</div>
+    </div>
   </div>
 </main>
 
