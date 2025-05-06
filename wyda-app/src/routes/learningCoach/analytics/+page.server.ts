ab221caff8f2bdc4325ddfaead65f9f8d02125fa
NoJc <nojac42@gmail.com>
2025-05-06 21:51:32 +1000

    refactor learning coach analytics to test backend stub

---
+++ wyda-app/src/routes/learningCoach/analytics/+page.server.ts
@@ -0,0 +1,22 @@
+import { chartdata } from "./data";
+
+
+export type ChartDataItem = {
+    chartLabel: string;
+    labels: string[];
+    backgroundColor: string[];
+    data: number[];
+}
+
+
+export function load (){
+    const processedValues: ChartDataItem[] = chartdata.map(item => ({
+        chartLabel: item.chartLabel,
+        labels: item.labels,
+        backgroundColor: item.backgroundColor,
+        data: item.data,
+    }));
+    return {
+        values: processedValues
+    };
+};
--- wyda-app/src/routes/learningCoach/analytics/+page.svelte
+++ wyda-app/src/routes/learningCoach/analytics/+page.svelte
@@ -1,56 +1,13 @@
-<script>
+<script lang="ts">
   import SideBarLearningCoach from "%lib/components/ui/side-bar/SideBarLearningCoach.svelte";
   import BarChart from "%lib/components/graphs/bar-chart/BarChart.svelte";
   import PieChart from "%lib/components/graphs/pie-chart/PieChart.svelte";
 
-  const pieChartdata = {
-    chartLabel: "User's Reflection Quality",
-    labels: ["low", "high"],
-    backgroundColor: ["rgba(222,90,90,1)", "rgba(180,221,221,1)"],
-    data: [300, 150],
-  };
-
-  const userProgressData = {
-    direction: "col",
-    chartLabel: "Overall User's Progress By Levels",
-    labels: ["Onboarding Activities", "Level Name", "Profitability"],
-    data: [12, 10, 5],
-    backgroundColor: ["rgb(180,221,221)"],
-  };
-
-  const countOfUsersByAttempts = {
-    chartLabel: "Count of Users by Attempts",
-    labels: [
-      "id1",
-      "id2",
-      "id3",
-      "id4",
-      "id5",
-      "id6",
-      "id7",
-      "id8",
-      "id9",
-      "id10",
-      "id11",
-      "id12",
-      "id13",
-    ],
-    backgroundColor: ["rgb(180,221,221)"],
-    data: [14, 6, 8, 9, 9, 2, 5, 11, 24, 9, 6, 8, 66],
-  };
-
-  const scenarioData = {
-    chartLabel: "Count of anonymous_id by scenario_name",
-    labels: [
-      "1.1 EVP",
-      "1.2 CFO",
-      "Company Breifing",
-      "N/A",
-      "Orientation to Simulation",
-    ],
-    backgroundColor: ["rgb(180,221,221)"],
-    data: [22, 10, 30, 70, 50],
-  };
+  let { data } = $props();
+  let userProgressData = data.values[0];
+  let scenarioData = data.values[1];
+  let countOfUsersByAttempts = data.values[2];
+  let pieChartData = data.values[3];
 </script>
 
 <main>
@@ -87,7 +44,7 @@
               <div class="chart-header font-semibold">
                 User's Reflection Quality
               </div>
-              <PieChart {...pieChartdata}></PieChart>
+              <PieChart {...pieChartData}></PieChart>
             </div>
           </div>
         </div>
