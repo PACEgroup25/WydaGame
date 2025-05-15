e8660854da48943b20dfe43d05a3a5cd093b9f60
NoJc <nojac42@gmail.com>
2025-05-15 19:31:03 +1000

    Define new slug routes for viewing specific cohort progress and user recent activity

--- wyda-app/src/routes/+layout.server.ts
+++ wyda-app/src/routes/+layout.server.ts
@@ -1,7 +1,19 @@
+import mysql from 'mysql2/promise';
+import {HOST} from '$env/static/private';
+
+const config = {
+    host:HOST,
+}
 
 export function load(){
     console.debug("test");
     return {
         info: "testdata"
     }
+}
+
+function fetch(){
+
+
+    let conn = mysql.createConnection(config);
 }
