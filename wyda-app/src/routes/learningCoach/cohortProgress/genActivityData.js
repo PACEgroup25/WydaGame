f6c39c6f8966c588f1b458372293ac057d65d4da
NoJc <nojac42@gmail.com>
2025-05-18 14:20:53 +1000

    Create small script to generate recentActivityData

---
+++ wyda-app/src/routes/learningCoach/cohortProgress/genActivityData.js
@@ -0,0 +1,25 @@
+import {faker} from '@faker-js/faker';
+
+function createData() {
+    return {
+        name: faker.person.fullName(),
+        org: faker.company.name(),
+        status: faker.helpers.arrayElement(['On track','Behind']),
+        challenge: "Challenge" + faker.number.int({max:5}).toString(),
+        reflectionQuality: faker.number.int({max:5}),
+        date: faker.date.recent()
+    }
+}
+
+let activityArr = [];
+
+function initData() {
+    for(var i = 0; i<100; i++){
+        activityArr.push(createData());
+    }
+    console.log(activityArr);
+    console.log(activityArr.length)
+}
+
+initData();
+
