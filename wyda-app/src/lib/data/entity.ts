af0de00e57e411d3d82aa2104ecc242664b7d214
samHoefnagels <samantha@getpacked.com.au>
2025-05-23 19:10:34 +1000

    role-based access almost complete
    - interfaces in Entity.ts edited for project needs
    - populateEntity class tested for functions necessary for role-based access

--- wyda-app/src/lib/data/entity.ts
+++ wyda-app/src/lib/data/entity.ts
@@ -3,30 +3,21 @@ export interface Entity { //end-user
 };
 
 export interface UserLinkedEntity extends Entity{ //user within database
-    entityID: string //RowKey / id in Users TABLE
+    entityID: string //RowKey
 };
 
 export interface EntityProfile extends Entity, UserLinkedEntity{
     firstName: string,
     lastName: string,
-    email: string,
     createdAt: Date,
     updatedAt: Date
 };
 
 export interface EntityHome extends Entity, EntityProfile{
-    cohortID: string | string[],
-    cohortName: string | string[],
-    organsationID: string,
-    organisationName: string
+    cohortID?: string | string[],
+    organisationID: string,
 };
 
 export interface EntityRole extends Entity, EntityProfile{
-    role : roleType
-};
-
-enum roleType{
-    learner = "Learner",
-    clientAdmin = "Client Admin",
-    learningCoach = "Learning Coach"
+    role : string
 };
