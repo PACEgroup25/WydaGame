53c07919c18c15f629e5180edf38faed521583cd
NoJc <nojac42@gmail.com>
2025-06-03 15:25:12 +1000

    Link both metrics and user data through user id in server before passing to page

--- wyda-app/src/routes/dev/return-cohort-users/[cohortID]/+page.server.ts
+++ wyda-app/src/routes/dev/return-cohort-users/[cohortID]/+page.server.ts
@@ -1,4 +1,5 @@
 import { BuildCohort } from '$lib/data/buildCohort.js';
+import type { EntityMetrics, EntityProfile } from '$lib/data/entity.js';
 
 export async function load({params}){
     const cohort = new BuildCohort(params.cohortID);
@@ -6,5 +7,28 @@ export async function load({params}){
     const cohortLearners = await cohort.getLearners();
     const cohortLearnersMetrics = await cohort.getLatestLearnerMetrics();
 
-    return {metaData: cohortData, learners: cohortLearners, learnerMetrics: cohortLearnersMetrics}
+    let metricsMap = new Map();
+
+    const learnerData: { profile: EntityProfile, metrics: EntityMetrics | undefined} [] = [];
+
+    //to link metrics to a user we rely on the userid
+
+    //make a map of metrics that are not undefined with user id as key
+    for(var i = 0; i<cohortLearnersMetrics.length; i++){
+        if(cohortLearnersMetrics[i]){
+            metricsMap.set(cohortLearnersMetrics[i]?.id,cohortLearnersMetrics[i])
+        }
+    }
+
+    //iterate through users, if id is present inside map, push their respective metrics in learnerData
+    //otherwise push profile and undefined
+    for(var i = 0; i<cohortLearners.length; i++){
+        if(metricsMap.get(cohortLearners[i].id)){
+            learnerData.push({profile: cohortLearners[i],metrics: metricsMap.get(cohortLearners[i].id)})
+        } else {
+            learnerData.push({profile: cohortLearners[i],metrics: undefined})
+        }
+    }
+
+    return {metaData: cohortData, learnerInfo: learnerData};
 }
--- wyda-app/src/routes/dev/return-cohort-users/[cohortID]/+page.svelte
+++ wyda-app/src/routes/dev/return-cohort-users/[cohortID]/+page.svelte
@@ -2,16 +2,19 @@
   import Button from "$lib/components/ui/button/button.svelte";
   let { data } = $props();
   console.log(data);
-  const learners = data.learners;
+  const learnerInfo = data.learnerInfo;
   const metaData = data.metaData;
 </script>
 
 <div class="flex flex-col justify-center items-center gap-10">
   {metaData.cohortName}
-  {#each learners as learner}
-    <Button href={"/dev/learners/" + learner.id}>
-      <div>name: {learner.firstName}</div>
-      <div>id: {learner.id}</div>
+  {#each learnerInfo as learner}
+    <Button href={"/dev/learners/" + learner.profile.id}>
+      <div>name: {learner.profile.firstName}</div>
+      <div>id: {learner.profile.id}</div>
     </Button>
+    {#if learner.metrics}
+      <div>status: {learner.metrics.userStatus}</div>
+    {/if}
   {/each}
 </div>
